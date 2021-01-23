704f7cc1e5ca76a1b47615b8cb8e8456a08f0fab
kylefarmer85 <kylefarmer85@gmail.com>
2021-01-22 21:01:52 -0600

    Add bcrypt and gravatar to users, correctly storing user in mongoDB atlas, user validations working

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
     });
 
     console.log('MongoDB Connected...');
