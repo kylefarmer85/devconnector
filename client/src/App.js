3ef52376c49807a36fbd653e202e0175682f73cf
kylefarmer85 <kylefarmer85@gmail.com>
2021-01-29 14:52:08 -0600

    create Posts, PostItem, posts properly fetch and display

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import { Provider } from 'react-redux';
@@ -59,6 +60,7 @@ const App = () => {
               path='/add-education'
               component={AddEducation}
             />
+            <PrivateRoute exact path='/posts' component={Posts} />
           </Switch>
         </section>
       </Router>
